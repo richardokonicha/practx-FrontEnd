0ff1b66935b60a77ba191836131e63e2eff22879
itstimiking <itstimiking@gmail.com>
2020-10-18 23:15:20 +0100

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-## PRACTX 
-PractX Web Front-End built with next.js [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
--- src/redux/saga.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-import authSaga from './auth/sagas';
-
-
-export default function* rootSaga(getState) {
-    yield all([
-        authSaga(),
-    ]);
-}
